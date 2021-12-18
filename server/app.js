8be5233bc22c73d38a25fa4ea9dca2c259752307
norufu <52996037+norufu@users.noreply.github.com>
2021-12-18 16:17:39 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
  	app.use(express.static('oto\build'));
   app.get('*', (request, response) => {
    console.log("getting *")
- 	 response.sendFile(path.join(__dirname, 'src/views', 'index.html'));
+ 	 response.sendFile(path.join(__dirname, 'src/views', 'Home.vue'));
   });
 }
 else {
