46686522f157a4cc38f4c165b9442fa4883b213f
norufu <52996037+norufu@users.noreply.github.com>
2022-01-17 18:54:21 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.static('/app/dist/'));
 if (process.env.NODE_ENV === 'production') {
   app.get('/', (request, response) => {
    console.log("getting *")
- 	 response.sendFile('/app/public/' + 'index.html');
+ 	 response.sendFile('index.html');
   });
 }
 else {
